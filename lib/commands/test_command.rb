96053613f2a818cd4e95ca4bd8239e077536d6e7
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2015-02-09 21:09:03 +0100

    Support running only unit tests

--- lib/commands/test_command.rb
+++ lib/commands/test_command.rb
@@ -1,13 +1,16 @@
 class TestCommand < Command
   def options
     {
-      "test" => "Run devbox-tools tests"
+      "test" => "Run devbox-tools tests",
+      "test:unit" => "Run devbox-tools unit tests",
     }
   end
 
-  def run(_)
+  def run(option)
     Finder.ruby_files("test").reverse.each do |path|
       next unless path.include?("tests.rb")
+      next if option == "test:unit" && !path.include?("unit")
+
       system("devbox-tools-ruby #{path}") || exit(1)
     end
   end
