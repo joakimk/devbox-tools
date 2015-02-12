13b2ab9a064f1295e4ca29e1ee088bbc9a9b2b91
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2015-02-12 20:46:18 +0100

    Unbreak git fixtures.

--- test/integration/git_test.rb
+++ test/integration/git_test.rb
@@ -1,4 +1,9 @@
 class TestGit < MTest::Unit::TestCase
+  def setup
+    unpack_repo fixture_path("git_repo_with_origin")
+    unpack_repo fixture_path("git_repo_without_origin")
+  end
+
   def test_returns_the_origin_url
     git_url = Git.origin_url(fixture_path("git_repo_with_origin"))
     assert_equal "git@github.com:joakimk/devbox-tools-fake-repo.git", git_url
@@ -14,5 +19,12 @@ class TestGit < MTest::Unit::TestCase
 
     git_url = Git.origin_url("/tmp")
     assert_nil git_url
+  end
+
+  private
+
+  # You can't add a git repo within a git repo.
+  def unpack_repo(path)
+    Shell.run("cp -rf #{path}/.git- #{path}/.git")
   end
 end
